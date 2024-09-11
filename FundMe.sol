ad2832a46716f6164e241dead27c2d1ae0b0bcff
Chirag-S-kotian <ckotian117@gmail.com>
2024-09-11 15:24:28 +0530

    FUNDME SOLIDITY

---
+++ FundMe.sol
@@ -0,0 +1,15 @@
+// get funds from users
+// withdraw funds 
+// set a minimum funding value in USD 
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract FundMe{
+    function fund() public payable{
+        // Allow users to send $
+        // Have a minimum $ sent 
+        // 1. How do we send ETH to this contract? 
+        require(msg.value > 1e18 , "didn't send enough ETH");
+    }
+}
